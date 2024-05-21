cd800f159fbf4c7b9f0737c9b4a931863233793e
Gleisson Braga <bragagleisson@gmail.com>
2024-05-21 20:24:32 -0300

    aprendendo retorno de funções

--- aula10_funcoes/retorno.js
+++ aula10_funcoes/retorno.js
@@ -20,3 +20,37 @@ function criarProduto( nome, preco) {
 const notebook = criarProduto("Notbook Intel Core i3 8GB", 2500)
 
 console.log(notebook)
+// ou
+console.log(criarProduto("Notbook Intel Core i3 8GB", 2500))
+
+function areaRetangular(base, altura) {
+    return base * altura
+}
+
+function areaQuadrada(lado) {
+    return areaRetangular(lado, lado)
+}
+
+// console.log(areaRetangular(3, 5))
+
+// console.log(areaQuadrada(9))
+
+function ola() {
+    let texto = "..."
+    return texto
+    texto = "Olá, mundo!"
+    console.log(texto)
+}
+
+console.log(ola())
+
+function maioridade(idade) {
+    if(idade >= 18) {
+        return "Maior de idade"
+    } else {
+        return "Menor de idade"
+    }
+}
+
+console.log(maioridade(29))
+console.log(maioridade(13))
