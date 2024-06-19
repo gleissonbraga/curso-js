0259b649a49662fdea053cbdc0058cfdffd6b933
Gleisson Braga <bragagleisson@gmail.com>
2024-06-19 06:59:06 -0300

    Desafio em babel e webpack

--- aula27_webpack/webpack.config.js
+++ aula27_webpack/webpack.config.js
@@ -1,3 +1,5 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
 
 module.exports = {
     entry: {
@@ -7,7 +9,10 @@ module.exports = {
     module: {
         rules: [{
             test: /\.css$/,
-            use: ['style-loader', 'css-loader']
+            use: [MiniCssExtractPlugin.loader, 'css-loader']
         }]
-    }
+    },
+    plugins: [
+        new MiniCssExtractPlugin()
+    ]
 }
