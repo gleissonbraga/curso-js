a4f9777262b8fcc21454bcc1163be5baad264763
Gleisson Braga <bragagleisson@gmail.com>
2024-06-19 07:13:52 -0300

    Aprendendo o webpack dev

--- desafio16/webpack.config.js
+++ desafio16/webpack.config.js
@@ -1,4 +1,13 @@
+const path = require('path')
+
 module.exports = {
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname, 'dist')
+        },
+        compress: true,
+        port: 7000 // muda a porta onde podemos colocar qualquer uma
+    },
     entry: {
         index: './src/index.js'
     },
