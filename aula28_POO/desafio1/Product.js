3ff65edb1ede274e324039b72e74e5403d3d3a2c
Gleisson Braga <bragagleisson@gmail.com>
2024-06-20 07:24:22 -0300

    Entrando em Poo

---
+++ aula28_POO/desafio1/Product.js
@@ -0,0 +1,12 @@
+class Product {
+    constructor(name, description, price, inStock) {
+        this.name = name
+        this.description = description
+        this.price = price
+        this.inStock = inStock
+    }
+
+    addToStock() {
+        
+    }
+}
