e4197aced9d20e1d98586c0f4d852eb7b2991c0d
Gleisson Braga <bragagleisson@gmail.com>
2024-06-24 07:22:15 -0300

    Entrando em herança

--- aula28_POO/desafio1/Product.js
+++ aula28_POO/desafio1/Product.js
@@ -3,10 +3,20 @@ class Product {
         this.name = name
         this.description = description
         this.price = price
-        this.inStock = inStock
+        this.inStock = 0
     }
 
-    addToStock() {
-        
+    addToStock(num) {
+        return this.inStock += num
+    }
+
+    calculateDiscount(discount) {
+        return this.price * ((100 - discount) / 100)
     }
 }
+
+const p1 = new Product("Pen", "Blue", "200", "20")
+
+console.log(p1)
+console.log(p1.addToStock(20))
+console.log(p1.calculateDiscount(10))
