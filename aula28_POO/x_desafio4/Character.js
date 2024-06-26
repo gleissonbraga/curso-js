d291deef5e20dc2add8aebb6d323c58cb6718a69
Gleisson Braga <bragagleisson@gmail.com>
2024-06-26 07:21:36 -0300

    Entrando em polimorfismo

---
+++ aula28_POO/x_desafio4/Character.js
@@ -0,0 +1,8 @@
+class Character {
+    constructor (name, health_points, attack_points, defense_points) {
+        this.name = name
+        this.health_points = health_points
+        this.attack_points = attack_points
+        this.defense_points = defense_points
+    }
+}
